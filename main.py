dd0ed00b31e2b7768f6e2dbf803cba2f4c53e162
rushigandhi <rushi.gandhi@hotmail.com>
2017-08-26 22:26:40 -0400

    feat(sentiment analysis): Working with any given Twitter name

--- main.py
+++ main.py
@@ -2,14 +2,46 @@ import tweepy
 import config
 from textblob import TextBlob
 
-auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
-auth.set_access_token(config.access_token, config.access_token_secret)
+consumer_key = config.consumer_key
+consumer_secret = config.consumer_secret
+access_token = config.access_token
+access_token_secret = config.access_token_secret
 
-api = tweepy.API(auth)
+def get_all_tweets(screen_name):
 
-public_tweets = api.search('A_harsimran')
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
 
-for tweet in public_tweets:
-    print(tweet.text)
-    analysis = TextBlob(tweet.text)
-    print(analysis.sentiment)
+    alltweets = []
+
+    new_tweets = api.user_timeline(screen_name = screen_name,count=200)
+
+    alltweets.extend(new_tweets)
+
+    oldest = alltweets[-1].id - 1
+
+    while len(new_tweets) > 0:
+        print("getting tweets before %s    " % (oldest))
+
+        new_tweets = api.user_timeline(screen_name = screen_name,count=200,max_id=oldest)
+
+        alltweets.extend(new_tweets)
+
+        oldest = alltweets[-1].id - 1
+
+        print ("...%s tweets downloaded so far" % (len(alltweets)))
+
+
+    for tweet in alltweets:
+        print(tweet.text)
+        analysis = TextBlob(tweet.text)
+        print(analysis.sentiment)
+
+
+
+if __name__ == '__main__':
+    print("What is your Twitter id?")
+    username = raw_input()
+    print("Hello " + username + " we will help you fix your Twitter! Please press enter!")
+    get_all_tweets(username)
